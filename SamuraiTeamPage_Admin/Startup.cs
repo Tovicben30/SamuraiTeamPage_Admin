a16ce11d40b91e950744e8d9a510f79ce3d4143a
Tovic Ben <bent@thomas.edu>
2018-11-19 16:58:58 -0500

    Add project files.

---
+++ SamuraiTeamPage_Admin/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SamuraiTeamPage_Admin.Startup))]
+namespace SamuraiTeamPage_Admin
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
